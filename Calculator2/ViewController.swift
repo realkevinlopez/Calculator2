f85cd4044c4c691533b30fea9e752bc5b76e1bda
realkevinlopez <kevin.medina.lopez@live.com>
2022-04-27 14:03:44 -0400

    Initial Commit

---
+++ Calculator2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by Kevin Lopez on 4/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
